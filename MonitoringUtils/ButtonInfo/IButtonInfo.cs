932e8363031acde9f92260bfe3f3656bc2533088
Vitoronga <vitorportella29@gmail.com>
2023-08-23 20:18:19 -0300

    Made interface for button info

---
+++ MonitoringUtils/ButtonInfo/IButtonInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringUtils.ButtonInfo
+{
+    public interface IButtonInfo
+    {
+        public bool IsPressed { get; init; }
+        public bool WasPressed { get; init; }
+    }
+}
